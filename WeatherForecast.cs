609297aaeff40f81055e1ae7526e12cb3c02768e
James Grieve <jamesonrgrieve@gmail.com>
2020-11-10 09:42:13 -0700

    In-class API practice;

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace ReactAPI_4Point2
 {
+    // In-class API practice:
+    // Add a "Precipitation" string property to the WeatherForecast class.
+    // Add a random generation of 5 precipitation descriptions to the API method (similar to the summaries).
+
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
